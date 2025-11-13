1e0e0292f2b5cfebee5c0b286a3d332a9ed2de27
Elias-Moral <elias.moral@edu.esiee.fr>
2025-11-13 10:22:50 +0000

    exercice palindrome

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
+""" exercice se servant des listes via la recherche de palindrome"""
 #### Fonction secondaire
 
 
 def ispalindrome(p):
-    table = str.maketrans("éèêâàäîïûüôöç", "eeeaaaiiuuooc")
-    table = str.maketrans("", "", " ")
-    p = texte.translate(table)
+    """paramaetre: p un string correspondant a notre chaine de charactere
+     return: boolean true ou false declarant si p est un palindrome """
+    p = p.lower()
+    table = str.maketrans("éèêëâàäîïûüôöç", "eeeeaaaiiuuooc")
+    table2 = str.maketrans("",""," _-,;:.?!'")
+    p = p.translate(table)
+    p = p.translate(table2)
 
+    print (p)
 
-    if (p[::-1]==p){
-        return true
-    }
-    else{
-        return False
-    }
+    return bool(p[::-1]==p)
+
 
 #### Fonction principale
 
 
 def main():
+    """fonction main"""
 
     # vos appels à la fonction secondaire ici
 
